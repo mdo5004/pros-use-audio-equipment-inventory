304834ca95f6bc45c2b5ce64cfbb300fa4426fe0
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-04-17 11:40:31 -0400

    Added log in form

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,10 +1,23 @@
 class SessionController < ApplicationController
     def new
+        
     end
     def create
+        @user = User.find_by(email: session_params[:email])
+        if @user && @user.authenticate(password: session_params[:password])
+            session[:user_id] = @user.id
+            redirect_to root_path 
+        else
+            render :new, :alert => "Could not log in"
+        end
     end
     
     def destroy
-        
+        session[:user_id] = nil
+    end
+    
+    private
+    def session_params
+        params.require(:session).permit(:email, :password)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
     root 'welcome#home'
 
+    get "/signin", to: "session#new"
+    post "/signin", to: "session#create"
+    delete "/signout", to: "session#destroy"
+    get "/signup", to: "users#new"
+    
     resources :items
     resources :rigs
     resources :users
