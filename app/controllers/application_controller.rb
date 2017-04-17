bd55b75d56286793fccb30c51d4495859e9d8bf4
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-04-17 15:40:38 -0400

    Rescue from not authorized error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,14 @@ class ApplicationController < ActionController::Base
     include Pundit
     protect_from_forgery with: :exception
 
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+    
     def current_user
         User.find(session[:user_id]) if session[:user_id] 
+    end
+    
+    def user_not_authorized
+        flash[:alert] = "Access denied." 
+        redirect_to(request.referrer || root_path)
     end
 end
