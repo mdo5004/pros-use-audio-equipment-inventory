94037e7159bded6a00e6345f739d201910c4ee14
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-04-17 11:23:27 -0400

    Basic User show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    
+
     def new
         @user = User.new
     end
@@ -22,7 +22,9 @@ class UsersController < ApplicationController
             render :edit
         end
     end
-    
+    def show
+        @user = User.find(params[:id])
+    end
     private
     def user_params
         params.require(:user).permit(:name, :email, :password, :title, :location)
