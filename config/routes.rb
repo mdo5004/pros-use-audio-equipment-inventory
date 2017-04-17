af0a7655b85d50f22ace59bbd395a9d8ff02d100
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-04-17 11:26:15 -0400

    Welcome controller redirects to user page if logged in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :items
-  resources :rigs
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    root 'welcome#home'
+
+    resources :items
+    resources :rigs
+    resources :users
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
