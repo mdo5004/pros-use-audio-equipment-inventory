197dc70e731b5d9761ee478a098ad36ec316e7dd
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-05-24 08:34:13 -0400

    Users can show/hide rig details

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,27 +2,33 @@ function Item(id, name){
     this.id = id;
     this.name = name;
 }
-    
+
 $(function(){
     $('.details-link').on('click', function(event){
         event.preventDefault();
-        
         let id = this["dataset"]["id"]
-        let $node = $("#panel-heading-" + id)
+        let $node = $("#panel-span-" + id)
+        var text = this.text 
         
-        
-        $.getJSON('/rigs/' + id, function(data){
-            let items = data["items"]
-            let item_array = []
-            items.forEach(function(item){
-                var i = new Item(item["id"],item["name"])
-                item_array.push(i)
-            })
-            var source   = $("#list-template").html();
-            var template = Handlebars.compile(source);
-//            debugger
+        if (text === "Show Details") {
+            this.text = "Hide Details";
 
-            $node.after(template({item: item_array}))    
-        })
+            $.getJSON('/rigs/' + id, function(data){
+                let items = data["items"]
+                let item_array = []
+                items.forEach(function(item){
+                    var i = new Item(item["id"],item["name"])
+                    item_array.push(i)
+                })
+                var source   = $("#list-template").html();
+                var template = Handlebars.compile(source);
+                //            debugger
+
+                $node.html(template({item: item_array}))    
+            })
+        } else {
+            this.text = "Show Details";
+            $node.html('')    
+        }
     })
 })
