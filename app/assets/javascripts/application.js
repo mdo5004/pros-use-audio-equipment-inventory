a94d48fca470cadd04e752e35519863966cccde4
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-06-08 18:43:11 -0400

    Trying to use babel to precompile js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,12 @@
 //= require handlebars
 //= require jquery-readyselector
 //= require_tree .
+var express = require('express');
+var app = express();
+var greeter = require('./greeter.js');
+
+app.get('/', function (req, res) {
+  res.send(greeter());
+});
+
+app.listen(process.env.PORT || 3000);
---
+++ app/assets/javascripts/greeter.js
@@ -0,0 +1,5 @@
+function greeter (msg = 'this works') {
+   return msg;
+};
+
+module.exports = greeter;
